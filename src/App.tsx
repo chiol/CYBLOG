c34bef736e12840e6aa3e382d1982e9c6724db58
치영 <>
2020-06-28 21:14:54 +0900

    add loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,36 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
-import { HomePage } from './pages';
 import './App.css';
+
+import { Spin } from 'antd';
+import { LoadingOutlined } from '@ant-design/icons';
+import styled from 'styled-components';
+
+
+
+
 function App() {
+  const HomePage = lazy(() => import("./pages/HomePage"));
+  const antIcon = <LoadingOutlined style={{ fontSize: 100}} spin />;
+
   return (
     <BrowserRouter>
-      <Switch>
-        <Route exact path="/CYBLOG" component={HomePage} />
-        <Route path="/CYBLOG/blog" component={()=>(<div style={{textAlign:"center", fontSize:"50px"}}>준비중...</div>)} />
-        <Redirect to="/CYBLOG"/>
-      </Switch>
+      <Suspense fallback={<Loading> <Spin indicator={antIcon} /> </Loading>}>
+        <Switch>
+          <Route exact path="/CYBLOG" component={HomePage} />
+          <Route path="/CYBLOG/blog" component={() => (<div style={{ textAlign: "center", fontSize: "50px" }}>준비중...</div>)} />
+          <Redirect to="/CYBLOG" />
+        </Switch>
+      </Suspense>
     </BrowserRouter>
   );
 }
+const Loading = styled.div `
+  display:flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100vh
+`
 
 export default App;
